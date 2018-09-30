0c13233e0517443a9b0bca29bcb3967b6695efce
fengdi <Fd88025442>
2018-09-30 18:05:41 +0800

    加入goEasy推送功能

--- src/test/java/com/fd/springbootdemo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/fd/springbootdemo/SpringbootDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fd.springbootdemo;
 
+import io.goeasy.GoEasy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,4 +14,9 @@ public class SpringbootDemoApplicationTests {
     public void contextLoads() {
     }
 
+    @Test
+    public void testGoEasy(){
+        GoEasy goEasy = new GoEasy("http://rest-hangzhou.goeasy.io","BC-54643d147f5a4ac289398680d36e5bdf");
+        goEasy.publish("my_channel","Hello, GoEasy!");
+    }
 }
